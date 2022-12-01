cde6825956ad12112fcf82704c91b26d44486040
Tom Sherman <the.tomsherman@gmail.com>
2022-12-01 09:42:37 +0000

    Day 1

--- src/day01.ts
+++ src/day01.ts
@@ -1,3 +1,27 @@
 export function solve(input: string, part: string) {
-  return "Hello";
+  switch (part) {
+    case "1":
+      return solvePart1(input);
+    case "2":
+      return solvePart2(input);
+  }
+}
+
+function solvePart1(input: string) {
+  return parseElveTotalCalories(input)[0];
+}
+
+function solvePart2(input: string) {
+  return parseElveTotalCalories(input).slice(0, 3).reduce((a, b) => a + b, 0);
+}
+
+function parseElveTotalCalories(input: string) {
+  const elves = input.trim().split("\n\n").map((elf) =>
+    elf.split("\n").map(Number)
+  );
+
+  return elves.map((calories) => calories.reduce((a, b) => a + b, 0)).sort((
+    a,
+    b,
+  ) => b - a);
 }
